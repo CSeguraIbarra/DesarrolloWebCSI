3d363927c32c45ccb8153c94b3418382df3846c6
CSeguraIbarra <81780765+CSeguraIbarra@users.noreply.github.com>
2023-12-01 20:56:29 -0400

    Maquetado examen final

---
+++ examenFinal/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div id="sub-menu">
+                <div><img src="images/escudo.png" alt="cargando" width="40px"></div>
+                <div class="btn1"><a class="enlaceop" href="">Opciones</a></div>
+                <div class="btn2"><a class="enlace" href="">Detalle 1</a></div>
+                <div class="btn2"><a class="enlace" href="">Detalle 2</a></div>
+            </div>
+            <div class="contentder">
+                <div id="menu">
+                    <div class="btn3"><a href="">Pregunta 1</a></div>
+                    <div class="lineaver"></div>
+                    <div class="btn3"><a href="">Pregunta 2</a></div>
+                    <div class="lineaver"></div>
+                    <div class="btn3"><a href="">Pregunta 3</a></div>
+                    <div class="lineaver"></div>
+                    <div class="btn3"><a href="">Pregunta 4</a></div>
+                    <div class="lineaver"></div>
+                    <div class="btn3"><a href="">Pregunta 5</a></div>
+                </div>
+                <div id="titulo">
+                    <div>Pregunta 1</div>
+                </div>
+                <div id="contenido">
+                    <div class="datos">
+                        <div class="datosmateria">
+                            <div>SIS 256 Programación Web</div>
+                            <div>Examen Final - 02-12-2023 7:00 am</div>
+                        </div>
+                        <div class="fotografia"><img src="images/estudiante.png" alt="cargando"></div>
+                        <div class="datosestudiante">
+                            <div class="negrita">Cristhian Segura Ibarra</div><br>
+                            <div class="datogray">Carrera: Ing. Sistemas</div>
+                            <div class="datogray">Repositorio Examen: </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="pie">
+            <div>Sucre-Semestre 2-2023</div>
+        </div>
+    </div>
+</body>
+
+</html>
